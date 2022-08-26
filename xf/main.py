7281fda4f7d69b7944d28ed9a6dd3e12df0f8762
callmeLP <343786790@qq.com>
2022-08-26 17:13:04 +0800

    add sth

--- xf/main.py
+++ xf/main.py
@@ -1,22 +1,14 @@
 import re
-import time
 import pyperclip
 from PIL import Image, ImageGrab,ImageTk
 import os
 from WebITRTeach import get_result
-import ctypes
-from tkinter import  messagebox, StringVar
+from tkinter import StringVar
 import tkinter as tk
 
 def start(textTemp, lab1, root_window):
-    # window_handle = ctypes.windll.kernel32.GetConsoleWindow()
-    # # 0：隐藏；6：最小化
-    # # 1：显示；3：最大化
-    # ctypes.windll.user32.ShowWindow(window_handle, 0)
-    # time.sleep(0.5)
     root_window.withdraw()
     os.system('RUNDLL32.EXE PrScrn.dll PrScrn')
-    # ctypes.windll.user32.ShowWindow(window_handle, 1)
     root_window.deiconify()
     img = ImageGrab.grabclipboard()
     if not isinstance(img, Image.Image):
@@ -27,8 +19,7 @@ def start(textTemp, lab1, root_window):
     recognition = get_result()
     res = recognition.call_url(image_path)
     photo = ImageTk.PhotoImage(Image.open(image_path))
-    lab1.config(image = photo)
-    lab1.img = photo
+    
     os.remove(image_path)
     if res['code'] != 0:
         return
@@ -37,6 +28,8 @@ def start(textTemp, lab1, root_window):
     content = re.sub(r'ifly-latex-end', '', content)
     content = re.sub(r'  ', '', content)
     textTemp.set(content)
+    lab1.config(image = photo)
+    lab1.img = photo
     # 将结果复制到剪切板
     pyperclip.copy(content)
     print(content)
@@ -49,7 +42,7 @@ if __name__ == '__main__':
     root_window.title("img2latex")
     textTemp = StringVar()
     lab1 = tk.Label(root_window)
-    button = tk.Button(root_window,text='截图',bg='#7CCD7C',command=lambda:start(textTemp, lab1, root_window )).pack()
+    button = tk.Button(root_window,text='截图',command=lambda:start(textTemp, lab1, root_window )).pack()
     lab1.pack()
     lab2 = tk.Label(root_window, textvariable = textTemp).pack()
     
